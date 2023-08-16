2ac223608b66d681a6953cc7785f8b831b2a7cee
Adam <adamgjohnson92@gmail.com>
2023-08-16 15:11:21 -0700

    Added elements that will handle combat.

--- js/main.js
+++ js/main.js
@@ -1,21 +1,23 @@
 import { characterRoster } from './character.js';
-import {charContainer, charCard, charAvatar, charNameDiv, charClassDiv, charHpDiv, charStrDiv, charDexDiv, charWisDiv, charHitDiv, charSpecial, equipCard, charWeaponName, charWeaponType, charWeaponWeight, charWeaponAttack1, charWeaponAttack2, charArmorName, charArmorClass, charArmorWeight, charArmorRating, charImgDiv} from './charCardElements.js';
-
+import { charContainer, charCard, charAvatar, charNameDiv, charClassDiv, charHpDiv, charStrDiv, charDexDiv, charWisDiv, charHitDiv, charSpecial, equipCard, charWeaponName, charWeaponType, charWeaponWeight, charWeaponAttack1, charWeaponAttack2, charArmorName, charArmorClass, charArmorWeight, charArmorRating, charImgDiv } from './charCardElements.js';
+const playBtn = document.getElementById('play-btn')
+const charSelectionDiv = document.getElementById('character-selection-div')
+const combatDiv = document.getElementById('combat-div')
 //--------------------------------------------------------------
 
 function selectCharacter(event) {
 
     let chosenCharacter;
 
-    for(let i = 0; i < characterRoster.length; i ++) {
-    if (event.target.matches('#Slick-play')) {
-        chosenCharacter = characterRoster[1]
-        console.log(chosenCharacter)
-    } else if (event.target.matches('#Vale-play')) {
-        chosenCharacter = characterRoster[0]
-        console.log(chosenCharacter)
+    for (let i = 0; i < characterRoster.length; i++) {
+        if (event.target.matches('#Slick-play')) {
+            chosenCharacter = characterRoster[1]
+            console.log(chosenCharacter)
+        } else if (event.target.matches('#Vale-play')) {
+            chosenCharacter = characterRoster[0]
+            console.log(chosenCharacter)
+        }
     }
-}
     charNameDiv.textContent = `${chosenCharacter.name}`
 
     charImgDiv.setAttribute("src", chosenCharacter.img)
@@ -38,19 +40,32 @@ function selectCharacter(event) {
     charArmorWeight.textContent = `Weight: ${chosenCharacter.armor.weight}`
     charArmorRating.textContent = `Damage Reduction: ${chosenCharacter.armor.armorRating}`
 
-    charContainer.style.display = "flex"
-}
+    charContainer.style.display = 'flex'
 
+    playBtn.style.display = 'block'
+};
 function generateCharBtns() {
-    const charSelectionDiv = document.getElementById('character-selection-div')
+
     for (let i = 0; i < characterRoster.length; i++) {
         const charBtn = document.createElement('button')
         charBtn.textContent = `${characterRoster[i].name} the ${characterRoster[i].charClass}`
         charBtn.setAttribute('class', 'btn character-btn')
-        charBtn.setAttribute('id', `${characterRoster[i].name}-play` )
+        charBtn.setAttribute('id', `${characterRoster[i].name}-play`)
         charSelectionDiv.append(charBtn)
         charBtn.addEventListener('click', selectCharacter)
     }
+};
+
+function playGame() {
+    charSelectionDiv.style.display = 'none'
+    combatDiv.style.display = 'flex'
+}
+
+playBtn.addEventListener('click', playGame)
+
+function attackRoll(stat){
+    const naturalRoll = Math.floor(Math.random() * (2 - 19) + 2)
+    totalRoll = naturalRoll + stat
 }
 
 generateCharBtns()
